710d29db82646e0b9c12c297dfde79b5c56fc519
Nik Gospodinov <nik.gospodinov@bt.com>
2024-01-04 09:49:15 +0000

    more changes

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,7 +1,12 @@
+import Link from "next/link";
+
 export default function About() {
   return (
-    <div className="bg-violet-950 text-violet-50">
-      <h1>This is the about page</h1>
+    <div className="bg-violet-950 text-violet-50 m-auto w-1/2 p-8">
+      <Link href="/">Home</Link>
+
+      <div className="h-10" />
+      <h1 className="text-4xl mb-2">This is the about page</h1>
     </div>
   );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,16 @@
+import Link from "next/link";
+
 export default function Home() {
   return (
-    <div className="bg-violet-950 text-violet-50">
-      <h1>This is the home page</h1>
-      <p>It is going to be served on AWS and used as an AWS playground</p>
+    <div className="bg-violet-950 text-violet-50 m-auto w-1/2 p-8">
+      <Link href="/about">About</Link>
+
+      <div className="h-10" />
+
+      <h1 className="text-4xl mb-2">This is the home page</h1>
+      <p className="text-xl">
+        It is going to be served on AWS and used as an AWS playground
+      </p>
     </div>
   );
 }
